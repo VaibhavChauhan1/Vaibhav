08ab4d979f2d7e4ffe357054f6491b370a99f601
VaibhavChauhan1 <94950064+VaibhavChauhan1@users.noreply.github.com>
2021-11-24 16:04:37 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,13 @@
+package area_and_parameter;
+
+public class main {
+	public static void main(String[] args) {
+		AreaAndPerimeter AnP=new AreaAndPerimeter();
+
+		AnP.areaandperimeter(7,5);
+		AnP.areaandperimeter(6.5);
+		AnP.areaandperimeter(8);
+		
+	}
+
+}
